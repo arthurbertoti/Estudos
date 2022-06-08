b88813aefa489748a2c1e218b1b788e76bb13de9
Arthur G. Bertoti <artgbertoti@gmail.com>
2022-06-08 18:29:30 -0300

    update desafio

--- javascript/desafios/adicionador_devalor/script.js
+++ javascript/desafios/adicionador_devalor/script.js
@@ -1,5 +1,16 @@
+var res = document.getElementById('res')
+var ray = []
 function adicionar(){
-    
-    let num = document.getElementById('numero')
-
+    let npt= document.getElementById('numero')
+    let num = Number(npt.value)
+    let slct = document.getElementById('slct')
+    if (num > 100 || num < 1){
+        window.alert('Número inválido!')
+    } else{
+        ray.push(num)
+        let item = document.createElement('option')
+        item.innerText = `Valor ${num} adicionado`
+        slct.appendChild(item)
+    }
+    res.innerHTML += (ray)
 }
