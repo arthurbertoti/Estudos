a3ca68341bc07a5bcd82d9c5479d4b4cc5dd827a
Arthur G. Bertoti <artgbertoti@gmail.com>
2022-07-16 00:15:24 -0300

    update day 2

---
+++ javascript/7DaysOfCode/day 2/day_2.js
@@ -0,0 +1,5 @@
+var nome =  prompt('Qual é o seu nome?');
+var idade = console.log('Quantos anos você tem?');
+var linguagem = console.log('Qual a linguagem de programação você está estudando?');
+
+console.log('Olá', nome, ', você tem ',idade,' anos e já está aprendendo ',linguagem,'!')
